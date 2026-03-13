f516dbd7b607df502135cf061760b8e161d1a2fe
John <john@Johns-MacBook-Air-2.local>
2026-03-13 15:10:09 +0800

    made a function slow print, defined some constants

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 ## Personal Programming Project John Tjiu
+import time
+import random
+from time import sleep
 
 
-# test
+def betterprint(text):
+    for character in text:
+        print(character, end = "", flush = True)
+        sleep(0.02)
+
+
+#card definitions
+DEFUSE = "🛡️•⩊•"
+NOPE = "🚫"
+ATTACK = "⚔️"
+SHUFFLE = "🔀"
+SKIP = "🏃"
+FAVOR = "🖤"
+SEE = "👀"
+KITTEN = "💣"
+
+CATS = ["🍉🐱", "🥔🐱", "🌈🐱"]
+
+
