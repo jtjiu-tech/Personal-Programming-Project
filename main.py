ca2f79aa60fbedef033e541949a2ed7f0bce2ada
(S) Tjiu <jtjiu@kellettschool.com>
2026-03-26 11:17:25 +0800

    I fixed previous functions and started making new functions for drawing cards

--- main.py
+++ main.py
@@ -37,14 +37,23 @@ class Player:
         self.hand = []
         self.alive = True
     def show_hand(self):
+        i = 0
         print(f"{self.name}, here are your cards: ")
         for item in self.hand:
-            print(item, end=", ")
+            i +=1
+            print(f"{i}. {item}")
 
 def create_deck():
     deck =([ATTACK] * 4 + [NOPE]*5 + [SHUFFLE]*4 + [SEE] * 5 + [FAVOR] * 4 + [SKIP] * 4 + CATS * 4)
     random.shuffle(deck)
     return deck
+
+def show_cards():
+    for player in players:
+        player.show_hand()
+        print("")
+        input("Press enter when done... ")
+        clear()
 
 def deal_card(players,deck):
     for player in players:
@@ -53,12 +62,46 @@ def deal_card(players,deck):
             card = deck.pop()
             player.hand.append(card)
         
-def draw_card():
-    #???
-    pass
+
+def getting_card():
+    print("Wait")
+    select = "Getting card..."
+    for i in range(5):
+        select += "."
+        print(select, end = "\r")
+        sleep(0.3)
+    sleep(1)
+    print(" ", len(select))
+    clear()
 
 
-def main():
+
+def player_turn(player,deck):
+    betterprint(f"{player.name}, what would you like to do?\n")
+    print("1) Draw a card")
+    print("2) Play a card")
+    choice = input("")
+    if choice == "1":
+        draw_card(player,deck)
+        if KITTEN in player.hand:
+            print("Youre deaddd!!!")
+            #do later
+    elif choice == "2":
+        pass
+    else:
+        pass
+
+def draw_card(player,deck):
+    card = deck.pop(0)
+    player.hand.append(card)
+    getting_card()
+    betterprint(f"You drew a {card}!!!\n")
+    sleep(2)
+    player.show_hand()
+    input("Done reading? Press Enter to move on...")
+    clear()
+
+def intro():
     clear()
     betterprint("-Welcome to EXPLODDDINGGG KITTTEEENNNSSSS 💣💣💣-\n")
     betterprint("Do you know how to play?? (yes or no) ")
@@ -68,8 +111,12 @@ def main():
     else:
         print("In this game, the goal is to be the last player standing. Every player has a total of 8 cards including a defuse at the beginning of the game. Once it is your turn, you have the ability to use one of your cards which have ability like: See the Future where you see the next 3 cards in the deck or you can directly draw a card from the pile. Unless you place a card that directly skips your turn, you have to draw a card from the deck. If you draw the exploding kitten, you will need to use a diffuse to survive, if you do not have one, then too bad… you're dead. Good luck and have fun!!!")
         sleep(5)
-        wipe = input("Done reading? Press Enter to move on...")
+        input("Done reading? Press Enter to move on...")
         clear()
+
+
+def main():
+    intro()
     
     numplayer = int(input("How many players are there? (2-4) "))
     while numplayer <2 or numplayer >4:
@@ -78,22 +125,20 @@ def main():
         name = input(f"Player {i+1}, what do I call you? ")
         players.append(Player(name))
     clear()
-
+    
     deck = create_deck()
     deal_card(players,deck)
+    show_cards()
 
     #add exploding kittens to deck after players recieved cards 
-    for i in range(numplayer - 1):
+    for i in range(numplayer-1):
         deck.append(KITTEN)
     random.shuffle(deck)
-    
-    #Show players what their cards are
-    for player in players:
-        player.show_hand()
-        print("")
-        input("Press enter when done... ")
-        clear()
-    print(deck)
+    while True:
+        for player in players:
+            player_turn(player,deck)
+        
 
+    
 
 main()
